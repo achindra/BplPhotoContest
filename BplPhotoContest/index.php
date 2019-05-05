cf61751246848c44f31000ada872aaa62fe76f96
Achindra Bhatnagar <achindra@github.com>
2019-05-05 10:52:33 +0530

    Bhopal Photo Contest

---
+++ BplPhotoContest/index.php
@@ -0,0 +1,85 @@
+<?php
+
+//echo "Hello World!";
+
+?>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: "Comic Sans MS";
+        }
+        .auto-style1 {
+            color: #FFFFCC;
+            font-size: x-large;
+        }
+        .newStyle2 {
+            font-family: "Comic Sans MS";
+        }
+        .auto-style2 {
+            background-color: #FFFFFF;
+        }
+        .auto-style3 {
+            font-family: "Comic Sans MS";
+            background-color: #FFFFFF;
+        }
+        .auto-style4 {
+            color: #FFFFCC;
+        }
+        .newStyle3 {
+            font-family: "Comic Sans MS";
+        }
+        .auto-style5 {
+            font-size: small;
+        }
+        .auto-style6 {
+            font-family: "Comic Sans MS";
+            font-size: x-large;
+        }
+    </style>
+</head>
+<body style="background-color:#FFCC00; width:760px">
+    <table style="width:100%; border:none">
+        <tr>
+            <td>
+                <img alt="#discoveryourbhopal" src="images/Icon.png"/>
+            </td>
+            <td>
+
+            </td>
+            <td style="text-align:center">
+                <span class="auto-style6">
+                #discoveryourbhopal</span>
+            </td>
+        </tr>
+    </table>
+    <table style="width:100%; border:none">
+        <tr>
+            <td>
+                <span class="newStyle1"><span class="auto-style1">do you love Bhopal?</span>
+                <br />
+                </span>
+                <br />
+                <span class="newStyle2"><span class="auto-style4">Discover the best of Bhopal and share with your friends!
+                <br />
+                </span>
+                <br />
+                Simply upload a photo and be lucky to stand a chance and win Movie Tickets for a Couple...
+                <br />
+                <br />
+                <span class="auto-style2"><a href="participate.php">PARTICIPATE</a> </span></span>
+                <p class="newStyle2"></p>
+                <p class="newStyle2"></p>
+                <span class="auto-style3"><a href="gallery.php">CLICK HERE TO VIEW GALLERY</a> </span>
+            </td>
+            <td>
+                <img alt="City In Pics" src="images/CityInPics.png" />
+            </td>
+        </tr>
+    </table>
+    <p style="text-align: right" class="auto-style5">
+        *T&amp;C Apply</p>
+</body>
+</html>
